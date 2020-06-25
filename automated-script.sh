0095fb14164c47a72b83483070ff3c9c1b6ada57
meet shah <meet.shah@dnata.com>
2020-06-25 22:05:29 +0400

    Added Git Ignore

--- automated-script.sh
+++ automated-script.sh
@@ -1,7 +1,7 @@
-docker build -t shahmeetk/nexus_demo .
-docker push shahmeetk/nexus_demo
+#docker build -t shahmeetk/nexus_demo .
+#docker push shahmeetk/nexus_demo
 cd terraform
-terraform init
+#terraform init
 terraform apply --auto-approve
 cd ..
 kubectl create -f deploy-nexus-kubernetes.yaml
